9504b2e3560a38eba45167849887a24013904386
diegocorado2 <113247911+diegocorado2@users.noreply.github.com>
2022-10-14 23:20:00 -0600

    Add files via upload

---
+++ SEMANA 9/T10_DECG_1003122/T10_DECG_1003122/CIRCULO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T10_DECG_1003122
+{
+    class CIRCULO
+    {
+        private double radio;
+        private double ObtenerPerimetro;
+        private double ObtenerArea;
+        private double ObtenerVolumen;
+        public double CalcularGeometria;
+
+
+
+        public CIRCULO(double radio_)
+        {
+            radio = radio_;
+
+        }
+
+        private double CalcularPerimetro()
+        {
+            double RESULTADOPERIMETRO = 2 * Math.PI * radio;
+            return RESULTADOPERIMETRO;
+
+        }
+
+        public double getperimetro()
+        {
+            return CalcularPerimetro();
+
+        }
+
+        private double calculararea()
+        {
+            double RESULTADOAREA = Math.PI * Math.Pow(radio, 2);
+            return RESULTADOAREA;
+        }
+
+        public double getarea()
+        {
+            return calculararea();
+        }
+
+        private double calcularvolumen()
+        {
+            double RESULTADOVOLUMEN = 4 * Math.PI * Math.Pow(radio, 3) % 3;
+            return RESULTADOVOLUMEN;
+        }
+
+        public double getvolumen()
+        {
+            return calcularvolumen();
+        }
+    }
+
+}
---
+++ SEMANA 9/T10_DECG_1003122/T10_DECG_1003122/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T10_DECG_1003122
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double radio = double.Parse(textBox1.Text);
+            CIRCULO _objcirculo = new CIRCULO(radio);
+            double resultado = _objcirculo.getperimetro();
+            MessageBox.Show(" EL RESUTADO DEL PERIMETRO ES: "+resultado.ToString(), "RESULTADO DEL PERIMETRO", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double radio = double.Parse(textBox1.Text);
+            CIRCULO _objcirculo = new CIRCULO(radio);
+            double resultado = _objcirculo.getarea();
+            MessageBox.Show(" EL RESUTADO DEL AREA ES: " + resultado.ToString(), "RESULTADO DEL AREA", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double radio = double.Parse(textBox1.Text);
+            CIRCULO _objcirculo = new CIRCULO(radio);
+            double resultado = _objcirculo.getvolumen();
+            MessageBox.Show(" EL RESUTADO DEL VOLUMEN ES: " + resultado.ToString(), "RESULTADO DEL VOLUMEN", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+    }
+}

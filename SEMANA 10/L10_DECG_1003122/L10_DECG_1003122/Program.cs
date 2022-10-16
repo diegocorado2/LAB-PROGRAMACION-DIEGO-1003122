83d098140b52385c4a66dd813171f71f0c5a8880
diegocorado2 <113247911+diegocorado2@users.noreply.github.com>
2022-10-15 20:35:12 -0600

    Add files via upload

---
+++ SEMANA 10/L10_DECG_1003122/L10_DECG_1003122/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace L10_DECG_1003122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo_Rectangulo triangulo = new Triangulo_Rectangulo();
+            Console.Write("INGRESE EL CATETO A: ");
+            triangulo.catetoA = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese El Angulo Opuesto a A: ");
+            triangulo.anguloOpuestoA = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("CATETO B: " + triangulo.ObtenerCatetoB());
+            Console.WriteLine("HIPOTENUSA: " + triangulo.ObtenerHipotenusa());
+            Console.WriteLine("ANGULO OPUESTO a B: " + triangulo.ObtenerAnguloOpuestoB());
+            Console.WriteLine("AREA DEL TRIANGULO: " + triangulo.ObtenerArea());
+
+        }
+    }
+}
---
+++ SEMANA 10/L10_DECG_1003122/L10_DECG_1003122/Triangulo_Rectangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L10_DECG_1003122
+{
+    class Triangulo_Rectangulo
+    {
+        public double catetoA;
+        public double anguloOpuestoA;
+
+        public double ObtenerCatetoB()
+        {
+            return catetoA / Math.Tan(anguloOpuestoA);
+        }
+
+        public double ObtenerHipotenusa()
+        {
+            return catetoA / Math.Sin(anguloOpuestoA);
+        }
+
+        public double ObtenerAnguloOpuestoB()
+        {
+            return 90 - anguloOpuestoA;
+        }
+
+        public double ObtenerArea()
+        {
+            return catetoA * ObtenerCatetoB();
+        }
+
+    }
+}
